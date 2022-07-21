8a110c42bb9d9f18d67db898bce2b7741c28437b
Darwin Lugo <developer.num@gmail.com>
2022-07-20 21:56:51 -0400

    Api Validacion de imagen

---
+++ Validator.Cliente/Controllers/ValidatorController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Validator.Cliente.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1.0")]
+    [ApiController]
+    public class ValidatorController : ControllerBase
+    {
+        // GET: api/<ValidatorController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValidatorController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValidatorController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValidatorController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValidatorController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Validator.Cliente/Models/ApplyMethods.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Validator.Cliente.Models
+{
+    public class RemoveVersionFromParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters.Count > 0)
+            {
+                var versionParameter = operation.Parameters.Single(v => v.Name.Equals("version"));
+                operation.Parameters.Remove(versionParameter);
+            }
+        }
+    }
+
+    public class ReplaceVersionWithExactValuePath : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            var paths = swaggerDoc.Paths;
+            swaggerDoc.Paths = new OpenApiPaths();
+
+            foreach (var path in paths)
+            {
+                var key = path.Key.Replace("v{version}", swaggerDoc.Info.Version);
+                var value = path.Value;
+                swaggerDoc.Paths.Add(key, value);
+            }
+        } 
+    }
+}
